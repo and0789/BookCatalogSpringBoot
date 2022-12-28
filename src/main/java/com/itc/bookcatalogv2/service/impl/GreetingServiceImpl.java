b8046a80457add35a98be7492a46a63e0e194ad1
andreseptian <andreseptian07@icloud.com>
2022-12-28 08:42:24 +0700

    Logging Configuration

--- src/main/java/com/itc/bookcatalogv2/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/itc/bookcatalogv2/service/impl/GreetingServiceImpl.java
@@ -3,12 +3,16 @@ package com.itc.bookcatalogv2.service.impl;
 import com.itc.bookcatalogv2.config.ApplicationProperties;
 import com.itc.bookcatalogv2.config.CloudProperties;
 import com.itc.bookcatalogv2.service.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.TimeZone;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
+
+    Logger log = LoggerFactory.getLogger(GreetingServiceImpl.class);
 
     private final ApplicationProperties applicationProperties;
 
@@ -21,6 +25,11 @@ public class GreetingServiceImpl implements GreetingService {
 
     @Override
     public String sayGreeting() {
+        log.trace("this is log TRACE");
+        log.debug("this is log DEBUG");
+        log.info("this is log INFO");
+        log.warn("this is log WARN");
+        log.error("this is log ERROR");
         System.out.println(cloudProperties.getApiKey());
         TimeZone timezone = TimeZone.getTimeZone(applicationProperties.getTimezone());
         return applicationProperties.getWelcomeText() + " , our timezone: " + timezone.getDisplayName() +
--- src/main/java/com/itc/bookcatalogv2/web/HelloResource.java
+++ src/main/java/com/itc/bookcatalogv2/web/HelloResource.java
@@ -1,11 +1,15 @@
 package com.itc.bookcatalogv2.web;
 
 import com.itc.bookcatalogv2.service.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
+
+    Logger log = LoggerFactory.getLogger(HelloResource.class);
 
     private final GreetingService greetingService;
 
@@ -16,6 +20,12 @@ public class HelloResource {
 
     @GetMapping("/hello")
     public String helloWorld() {
+        log.trace("this is log TRACE");
+        log.debug("this is log DEBUG");
+        log.info("this is log INFO");
+        log.warn("this is log WARN");
+        log.error("this is log ERROR");
+
         return greetingService.sayGreeting();
     }
 
