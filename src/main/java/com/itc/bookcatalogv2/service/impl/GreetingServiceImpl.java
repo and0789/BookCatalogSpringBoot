a071b6edf4c91a1dbbc11bb113d3aa97d5b7de5a
andreseptian <andreseptian07@icloud.com>
2022-12-28 08:09:09 +0700

    Read Property From EV SO

--- src/main/java/com/itc/bookcatalogv2/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/itc/bookcatalogv2/service/impl/GreetingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.itc.bookcatalogv2.service.impl;
 
 import com.itc.bookcatalogv2.config.ApplicationProperties;
+import com.itc.bookcatalogv2.config.CloudProperties;
 import com.itc.bookcatalogv2.service.GreetingService;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,16 @@ public class GreetingServiceImpl implements GreetingService {
 
     private final ApplicationProperties applicationProperties;
 
-    public GreetingServiceImpl(ApplicationProperties applicationProperties) {
+    private final CloudProperties cloudProperties;
+
+    public GreetingServiceImpl(ApplicationProperties applicationProperties, CloudProperties cloudProperties) {
         this.applicationProperties = applicationProperties;
+        this.cloudProperties = cloudProperties;
     }
 
     @Override
     public String sayGreeting() {
+        System.out.println(cloudProperties.getApiKey());
         TimeZone timezone = TimeZone.getTimeZone(applicationProperties.getTimezone());
         return applicationProperties.getWelcomeText() + " , our timezone: " + timezone.getDisplayName() +
                 ", our currency: " + applicationProperties.getCurrency();
